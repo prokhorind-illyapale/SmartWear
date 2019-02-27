851c6f8e1d9021d7c34cdc75bcd64c050caaf559
prokhorind <den.u@mail.ru>
2019-02-28 00:17:40 +0200

    h2 config

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,51 @@
+INSERT INTO role (role_id, name) VALUES (1, 'USER');
+INSERT INTO role (role_id, name) VALUES (2, 'ADMIN');
+
+INSERT INTO user (user_id, city, email, login, password, role_id, sex) VALUES
+  (1, 'kyiv', 'prokhorind@gmail.com', 'testuser2', '$2a$10$hMiN1v.ymyS8fQvIl/O7P.5BNFnrdB3f6so4EhCIQDd2/0OgxcbbC', 1,
+   'M');
+INSERT INTO user (user_id, city, email, login, password, role_id, sex) VALUES
+  (2, 'kyiv', 'prokhorind@gmail.com', 'testuser1', '$2a$10$2eOu8jADdkPOunPyrPy13.6Pypv9jBRSagFMe05Gm5k5ljzqpLn3W', 2,
+   'M');
+INSERT INTO sex (sex_id, name) VALUES ('1', 'm');
+INSERT INTO sex (sex_id, name) VALUES ('2', 'f');
+INSERT INTO sex (sex_id, name) VALUES ('3', 'u');
+
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('1', 'Head');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('2', 'High ');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('3', 'Middle');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('4', 'Low');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('5', 'Legs');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('6', 'Gloves');
+INSERT INTO cloth_type (cloth_type_id, name) VALUES ('7', 'Shoes');
+
+INSERT INTO cloth (cloth_id, name, cloth_type_id, sex_id) VALUES ('1', 'jeans', '5', '1');
+INSERT INTO cloth (cloth_id, name, cloth_type_id, sex_id) VALUES ('2', 'jeans', '5', '2');
+INSERT INTO cloth (cloth_id, name, cloth_type_id, sex_id) VALUES ('3', 'Cap', '1', '3');
+
+
+INSERT INTO look_type (look_type_id, name) VALUES ('1', 'Formal');
+INSERT INTO look_type (look_type_id, name) VALUES ('2', 'Unformal');
+INSERT INTO look_type (look_type_id, name) VALUES ('3', 'Sport');
+INSERT INTO look_type (look_type_id, name) VALUES ('4', 'Party');
+INSERT INTO look_type (look_type_id, name) VALUES ('5', 'Work');
+
+INSERT INTO look (look_id, description, likes, user_id, is_active, min_temperature, max_temperature, code)
+VALUES (1, 'checklook1 for testuser2', 0, 1, 1, 10, 15, '1');
+INSERT INTO look (look_id, description, likes, user_id, is_active, min_temperature, max_temperature, code)
+VALUES (2, 'checklook2 for testuser2', 0, 1, 0, 9, 20, '2');
+
+INSERT INTO look_to_look_type (look_id, look_type_id) VALUES ('1', '1');
+INSERT INTO look_to_look_type (look_id, look_type_id) VALUES ('1', '5');
+INSERT INTO look_to_look_type (look_id, look_type_id) VALUES ('2', '2');
+INSERT INTO look_to_look_type (look_id, look_type_id) VALUES ('2', '4');
+
+INSERT INTO user_cloth_attribute (user_cloth_id, code, color, description, picture, price, size, cloth_id, user_id)
+VALUES (1, '1', 'green', 'customize cloth', NULL, 100.00, 'M', 1, 1);
+
+INSERT INTO look_to_user_clothes (look_id, user_cloth_id) VALUES (1, 1);
+
+INSERT INTO comment (comment_id, last_updated, login, message, look_id)
+VALUES (1, '2019-02-24 00:00:00', 'Валера', 'Hi', 2);
+INSERT INTO comment (comment_id, last_updated, login, message, look_id)
+VALUES (2, '2019-02-24 00:00:00', 'Hi', 'Валера', 2);
