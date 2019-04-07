7cb003a8e60a27c75044e780da0895e32603d485
prokhorind <den.u@mail.ru>
2019-04-07 23:27:32 +0300

    Add get request for cloth type .

--- server/src/main/java/ua/javaee/springreact/web/controller/ClothTypeController.java
+++ server/src/main/java/ua/javaee/springreact/web/controller/ClothTypeController.java
@@ -1,7 +1,18 @@
 package ua.javaee.springreact.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ua.javaee.springreact.web.converter.ClothTypeDataToFormConverter;
+import ua.javaee.springreact.web.data.ClothTypeData;
+import ua.javaee.springreact.web.facade.ClothTypeFacade;
+import ua.javaee.springreact.web.form.lookforms.ClothTypeDataForm;
+
+import java.security.Principal;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * Created by kleba on 24.02.2019.
@@ -10,5 +21,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/cloth/type")
 public class ClothTypeController {
 
+    @Autowired
+    private ClothTypeFacade clothTypeFacade;
+    @Autowired
+    private ClothTypeDataToFormConverter converter;
 
+    @GetMapping
+    public ResponseEntity<List<ClothTypeDataForm>> getClothTypes(Principal principal) {
+        List<ClothTypeData> clothTypeDatas = clothTypeFacade.getAllClothTypes();
+        return ok(clothTypeDatas.stream().map(this::convert).collect(toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveClothType(ClothTypeDataForm form, Principal principal) {
+        return ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateClothType(Principal principal) {
+
+        return ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity removeClothType(Principal principal) {
+
+        return ok().build();
+    }
+
+    private ClothTypeDataForm convert(ClothTypeData clothType) {
+        return converter.convert(clothType);
+    }
 }
---
+++ server/src/main/java/ua/javaee/springreact/web/service/ClothTypeService.java
@@ -0,0 +1,12 @@
+package ua.javaee.springreact.web.service;
+
+import ua.javaee.springreact.web.entity.ClothType;
+
+import java.util.List;
+
+/**
+ * Created by kleba on 07.04.2019.
+ */
+public interface ClothTypeService {
+    List<ClothType> getAllClothTypes();
+}
