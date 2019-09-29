802c12d0fb504d4618319d1187b79a1d011bfe78
Denys Prokhorin <prokhorind@gmail.com>
2019-09-29 22:11:16 +0300

    Add logic for adding /removing commands from device

--- server/src/main/java/ua/javaee/springreact/web/controller/CommandController.java
+++ server/src/main/java/ua/javaee/springreact/web/controller/CommandController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import ua.javaee.springreact.web.data.CommandData;
 import ua.javaee.springreact.web.facade.UserFacade;
 import ua.javaee.springreact.web.facade.impl.DefaultCommandFacade;
+import ua.javaee.springreact.web.facade.impl.DefaultDeviceFacade;
 
 import java.security.Principal;
 
@@ -22,8 +23,12 @@ public class CommandController {
     public static final String NO_RIGHTS = "You don't have rights for this action";
     public static final String COMMAND_ALREADY_EXISTS = "Command already exists";
     public static final String COMMAND_NOT_EXISTS = "Command not exists";
+    public static final String DEVICE_CONTAINS_COMMAND = "Device already contains this command";
+    public static final String DEVICE_NO_COMMAND = "Device doesn't contains command";
     @Autowired
     private DefaultCommandFacade defaultCommandFacade;
+    @Autowired
+    private DefaultDeviceFacade defaultDeviceFacade;
     @Autowired
     private UserFacade userFacade;
 
@@ -45,6 +50,32 @@ public class CommandController {
             return processingErrors(COMMAND_ALREADY_EXISTS, VALIDATION_TYPE_ERROR);
         }
         defaultCommandFacade.add(command);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("{commandName}/device/{deviceName}")
+    public ResponseEntity addCommandToDevice(@PathVariable String commandName, @PathVariable String deviceName, Principal principal) {
+        if (!userFacade.isUserHasAdminRights(principal.getName())) {
+            return processingErrors(NO_RIGHTS, PERMISSION_TYPE_ERROR);
+        }
+
+        if (defaultDeviceFacade.isDeviceContainsCommand(commandName, deviceName)) {
+            return processingErrors(DEVICE_CONTAINS_COMMAND, VALIDATION_TYPE_ERROR);
+        }
+        defaultDeviceFacade.addCommand(commandName, deviceName);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("{commandName}/device/{deviceName}")
+    public ResponseEntity deleteCommandFromDevice(@PathVariable String commandName, @PathVariable String deviceName, Principal principal) {
+        if (!userFacade.isUserHasAdminRights(principal.getName())) {
+            return processingErrors(NO_RIGHTS, PERMISSION_TYPE_ERROR);
+        }
+
+        if (!defaultDeviceFacade.isDeviceContainsCommand(commandName, deviceName)) {
+            return processingErrors(DEVICE_NO_COMMAND, VALIDATION_TYPE_ERROR);
+        }
+        defaultDeviceFacade.deleteCommand(commandName, deviceName);
         return ResponseEntity.ok().build();
     }
 
--- server/src/main/java/ua/javaee/springreact/web/facade/impl/DefaultDeviceFacade.java
+++ server/src/main/java/ua/javaee/springreact/web/facade/impl/DefaultDeviceFacade.java
@@ -8,6 +8,7 @@ import ua.javaee.springreact.web.entity.Device;
 import ua.javaee.springreact.web.service.impl.DefaultDeviceService;
 
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.Objects.isNull;
 import static java.util.stream.Collectors.toList;
@@ -21,6 +22,22 @@ public class DefaultDeviceFacade {
 
     public void add(DeviceData deviceData) {
         defaultDeviceService.add(deviceData);
+    }
+
+    public void addCommand(String commandName, String deviceName) {
+        defaultDeviceService.addCommandToDevice(commandName, deviceName);
+    }
+
+    public void deleteCommand(String commandName, String deviceName) {
+        defaultDeviceService.deleteCommandFromDevice(commandName, deviceName);
+    }
+
+    public boolean isDeviceContainsCommand(String commandName, String deviceName) {
+        Device device = defaultDeviceService.findByName(deviceName);
+        if (Objects.isNull(device)) {
+            return false;
+        }
+        return device.getCommands().stream().anyMatch(c -> commandName.equalsIgnoreCase(c.getName()));
     }
 
     public void remove(DeviceData deviceData) {
